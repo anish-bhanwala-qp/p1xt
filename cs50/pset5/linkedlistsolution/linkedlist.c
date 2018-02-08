de25509ae5ad1ac86b327c738e6df52ace371118
Anish Bhanwala <anish.bhanwala@questionpro.com>
2018-02-08 17:05:39 +0530

    Create linkedlist.c

---
+++ cs50/pset5/linkedlistsolution/linkedlist.c
@@ -0,0 +1,126 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "linkedlist.h"
+
+void addnode(linkedlist *ll, char *word)
+{
+    //Create newnode and allocate memory
+    node *newnode = malloc(sizeof(node)); ;
+    newnode->next = NULL;
+    newnode->word = malloc(sizeof(char) * 46);
+    strcpy(newnode->word, word);
+
+    //printf("word: %s, copy: %s\n", word, newnode->word);
+
+    node *lastnode = getlastnode(ll);
+    if (lastnode != NULL)
+    {
+        //set newnode as last node
+        lastnode->next = newnode;
+    }
+    else
+    {
+        //set newnode as first node
+        ll->first = newnode;
+    }
+}
+
+node *getlastnode(linkedlist *ll)
+{
+    //get first node
+    node *last = ll->first;
+
+    //loop until last is NULL or last node doesn't point to any node
+    while (last != NULL && last->next != NULL)
+    {
+        last = last->next;
+    }
+
+    return last;
+}
+
+bool freememory(linkedlist *ll)
+{
+    node *n = ll->first;
+    node *prev;
+    while (n != NULL)
+    {
+        //printf("free memory: %s\n", n->word);
+        n = (*n).next;
+        prev = n;
+        if (prev != NULL)
+        {
+            free(prev->word);
+            free(prev);
+        }
+    }
+
+    free(ll);
+
+    return true;
+}
+
+bool findword(linkedlist *ll, const char *word)
+{
+    //Make sure word is lower case
+    char copiedword[45 + 1];
+    strcpy(copiedword, word);
+    tolowercase(copiedword);
+
+    node *n = ll->first;
+    while (n != NULL)
+    {
+        if (strcmp(copiedword, n->word) == 0)
+        {
+            //printf("word found %s\n", word);
+            return true;
+        }
+        n = n->next;
+    }
+
+    //printf("word not found %s\n", word);
+    return false;
+}
+
+void printall(linkedlist *ll)
+{
+    int i = 1;
+    node *n = (*ll).first;
+    while (n != NULL && (*n).next != NULL)
+    {
+        //printf("node %i, %p, %s\n", i, n, (*n).word);
+        n = (*n).next;
+        i++;
+
+        if (i == 10)
+        {
+            break;
+        }
+    }
+    //printf("node %i, %p, %s\n", i, n, (*n).word);
+}
+
+void tolowercase(char *word)
+{
+    for(int i = 0; i < strlen(word); i++)
+    {
+        word[i] = tolower(word[i]);
+    }
+}
+
+int sizeoflinkedlist(linkedlist *ll)
+{
+    int i = 0;
+    node *n = (*ll).first;
+    while (n != NULL)
+    {
+        i++;
+        n = (*n).next;
+    }
+
+    return i;
+}
